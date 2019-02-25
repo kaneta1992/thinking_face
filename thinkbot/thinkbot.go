34d08e05684faa39c5cbc5857ab5a26d9bc77075
kaneta <kaneta1011@gmail.com>
2019-02-25 22:42:36 +0900

    Genarate tweet message

--- thinkbot/thinkbot.go
+++ thinkbot/thinkbot.go
@@ -1,6 +1,9 @@
 package thinkbot
 
 import (
+	"math/rand"
+	"strings"
+
 	"github.com/ChimeraCoder/anaconda"
 	"github.com/kaneta1992/thinking_face_bot/helper"
 )
@@ -17,6 +20,10 @@ func getRandomMediaPath() string {
 	return helper.RandomSelect(helper.DirWalk("./media"))
 }
 
+func generateMessage() string {
+	return strings.Repeat("🤔", rand.Intn(10)+1)
+}
+
 func (bot *ThinkBot) StartReplyBot() {
 	go func() {
 		s := bot.wrapper.GetTrackPublicStreamFilter("@thinkbott")
@@ -25,7 +32,7 @@ func (bot *ThinkBot) StartReplyBot() {
 			case anaconda.Tweet:
 				tweet := v
 				go func() {
-					_, err := bot.wrapper.ReplyWithMedia("thikning...", getRandomMediaPath(), tweet)
+					_, err := bot.wrapper.ReplyWithMedia(generateMessage(), getRandomMediaPath(), tweet)
 					helper.CheckIfError(err)
 				}()
 			}
@@ -35,7 +42,7 @@ func (bot *ThinkBot) StartReplyBot() {
 
 func (bot *ThinkBot) StartTweetBot() {
 	go func() {
-		_, err := bot.wrapper.TweetWithMedia("", getRandomMediaPath())
+		_, err := bot.wrapper.TweetWithMedia(generateMessage(), getRandomMediaPath())
 		helper.CheckIfError(err)
 	}()
 }
